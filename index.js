9bbfe64d070c3c5706ff5cd4cd5f92830a0f4762
JSJm25 <jeffreymacmullin@gmail.com>
2024-12-18 12:27:59 -0800

    added mongoDB, made .env file private

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-require('dotenv').config();
+
 const express = require('express');
-const cors = require('cors');
 const app = express();
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
+const cors = require('cors');
+
+require('dotenv').config();
+const port = process.env.PORT;
+
+const { MongoClient } = require('mongodb');
+const client = new MongoClient();
+
+let myDir = __dirname + "/views/index.html";
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+
+app.use((req, res, done) =>{
+  let d = new Date().toUTCString();
+  console.log(`${req.method}    ${req.path} - ${req.ip} at ${d}`);
+  done();
+});
+
 app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(myDir);
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/shorturl', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
+  console.log(`https://localhost:${port}/`);
 });
